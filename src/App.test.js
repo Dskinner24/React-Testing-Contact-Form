392ce7b0a1d220931c477994c91054c51670ff5c
unknown <dskinner_93@yahoo.com>
2020-07-17 16:31:28 -0700

    Jest Tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,38 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent, getByLabelText, getByTestId, getByPlaceholderText } from "@testing-library/react";
 import App from "./App";
 
 test("renders App without crashing", () => {
   render(<App />);
 });
+
+test("contains place holders", () => {
+  const { getByPlaceholderText } = render(<App />);
+
+  getByPlaceholderText(/edd/i);
+  getByPlaceholderText(/burke/i);
+});
+
+test("contains submit button", () => {
+  const { getByTestId } = render(<App />)
+
+  getByTestId("submit")
+})
+
+test("contains correct labels", () => {
+  const { getByLabelText } = render(<App />)
+
+  getByLabelText(/Message/i);
+  getByLabelText(/Email*/i);
+  getByLabelText(/Last Name*/i);
+  getByLabelText(/First Name*/i)
+})
+
+test("first name input", () => {
+  const { getByLabelText } = render(<App />)
+
+  let firstInput = getByLabelText("First Name*");
+
+  expect(firstInput.textContent).toBe("");
+})
+
